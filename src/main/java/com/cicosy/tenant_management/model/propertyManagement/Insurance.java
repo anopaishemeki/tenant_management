4677b4cfd3c7cc47d61f98256b9952c2fe330a7e
Mpumelelodube <dubempumelelo9@gmail.com>
2022-02-01 18:22:29 +0200

    added Address.java, ContactDetails.java Owner.java, Insurance.java

---
+++ src/main/java/com/cicosy/tenant_management/model/propertyManagement/Insurance.java
@@ -0,0 +1,9 @@
+package com.cicosy.tenant_management.model.propertyManagement;
+
+public class Insurance {
+    private String id;
+    private String providerName;
+    private String packageName;
+    private double subscriptionFee;
+    private Long contactDetails;
+}
--- src/main/java/com/cicosy/tenant_management/model/propertyManagement/Property.java
+++ src/main/java/com/cicosy/tenant_management/model/propertyManagement/Property.java
@@ -24,18 +24,28 @@ public class Property {
     )
     private Long id;
     private String name;
-    private String address;
+    private Long address;
     private String tenant;
+    private Long insurance;
     private String description;
     private String propertyType;
     private String city;
-    private String owner;
+    private Long owner;
     private String province;
     private String status;
+    private double assetValue;
 
-    public Property(String name, String address, String tenant) {
+    public Property(String name, Long address, String tenant, Long insurance, String description, String propertyType, String city, Long owner, String province, String status, double assetValue) {
         this.name = name;
         this.address = address;
         this.tenant = tenant;
+        this.insurance = insurance;
+        this.description = description;
+        this.propertyType = propertyType;
+        this.city = city;
+        this.owner = owner;
+        this.province = province;
+        this.status = status;
+        this.assetValue = assetValue;
     }
 }
