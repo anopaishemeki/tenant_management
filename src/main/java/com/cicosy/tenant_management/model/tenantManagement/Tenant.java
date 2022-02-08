b6624381200324d55355cf9d53bf32fce81fc820
sudo-tafadzwa <peterx324@gmail.com>
2022-02-08 09:19:03 +0200

    Lease Management Module with error

--- src/main/java/com/cicosy/tenant_management/model/tenantManagement/Tenant.java
+++ src/main/java/com/cicosy/tenant_management/model/tenantManagement/Tenant.java
@@ -1,10 +1,13 @@
 package com.cicosy.tenant_management.model.tenantManagement;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -25,18 +28,27 @@ public class Tenant {
     private Long id;
 
     private String name;
-    private String contact_details;
+    private  String surname;
+
+    private String email;
+    private String phone;
+    private String id_passport;
     private String residential_address;
     private String lease;
     private String property;
 
-    public Tenant(String name, String contact_details,
-                  String residential_address,
+
+    public Tenant(String name,String surname, String email, String phone,
+                  String id_passport, String residential_address,
                   String lease, String property) {
         this.name = name;
-        this.contact_details = contact_details;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.id_passport = id_passport;
         this.residential_address = residential_address;
         this.lease = lease;
         this.property = property;
     }
+
 }
