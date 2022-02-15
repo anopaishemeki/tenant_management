ac5d54226c332ae6cbed13771b3470d0a51d3293
Mpumelelodube <dubempumelelo9@gmail.com>
2022-02-15 09:10:30 +0200

    update to compartment api

--- src/main/java/com/cicosy/tenant_management/repository/propertyManagement/CompartmentRepository.java
+++ src/main/java/com/cicosy/tenant_management/repository/propertyManagement/CompartmentRepository.java
@@ -3,9 +3,13 @@ package com.cicosy.tenant_management.repository.propertyManagement;
 import com.cicosy.tenant_management.model.propertyManagement.Compartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface CompartmentRepository extends JpaRepository<Compartment, Long> {
+    @Query("from Compartment where property = id")
+    List<Compartment> findByProperty(@Param("id") Long id);
 }
--- src/main/java/com/cicosy/tenant_management/service/propertyManagement/CompartmentService.java
+++ src/main/java/com/cicosy/tenant_management/service/propertyManagement/CompartmentService.java
@@ -5,6 +5,8 @@ import com.cicosy.tenant_management.repository.propertyManagement.CompartmentRep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CompartmentService {
     CompartmentRepository compartmentRepository;
@@ -16,5 +18,18 @@ public class CompartmentService {
 
     public void save(Compartment compartment){
         compartmentRepository.save(compartment);
+    }
+
+    public Compartment getCompartment(Long id) {
+        return compartmentRepository.findById(id).orElseThrow(() -> new  IllegalStateException ("Compartment with id: " + id +" does not exist"));
+
+    }
+
+    public List<Compartment> getCompartments() {
+        return compartmentRepository.findAll();
+    }
+
+    public List<Compartment> getCompartmentsForSpecificProperty(Long id) {
+        return compartmentRepository.findByProperty(id);
     }
 }
