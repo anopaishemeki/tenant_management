93d8aa6c4f1d34ebcaf665435d47083a0639d9a7
Mpumelelodube <dubempumelelo9@gmail.com>
2022-03-10 16:50:43 +0200

    payment module update

--- src/main/java/com/cicosy/tenant_management/controler/accounting/PaymentController.java
+++ src/main/java/com/cicosy/tenant_management/controler/accounting/PaymentController.java
@@ -1,5 +1,7 @@
 package com.cicosy.tenant_management.controler.accounting;
 
+import com.cicosy.tenant_management.controler.propertyManagement.CompartmentController;
+import com.cicosy.tenant_management.controler.tenantManagement.TenantController;
 import com.cicosy.tenant_management.model.accounting.Payment;
 import com.cicosy.tenant_management.service.accounting.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +13,40 @@ import java.util.List;
 @RequestMapping("/api/payment")
 public class PaymentController {
     private final PaymentService paymentService;
+    private final CompartmentController compartmentController;
+    private final com.cicosy.tenant_management.controler.propertyManagement.propertyController propertyController;
+    private final TenantController tenantController;
 
     @Autowired
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, CompartmentController compartmentController, com.cicosy.tenant_management.controler.propertyManagement.propertyController propertyController, TenantController tenantController) {
         this.paymentService = paymentService;
+        this.compartmentController = compartmentController;
+        this.propertyController = propertyController;
+        this.tenantController = tenantController;
     }
 
-    @PostMapping("/save-payment/{id}")
+    @PostMapping("/save-payment")
     public Payment savePayment(@RequestBody Payment payment){
+        System.out.println(payment.toString());
         return paymentService.save(payment);
     }
 
     @GetMapping("/get-payments")
     public List<Payment> getPayments(){
-        return paymentService.getAll();
+        List<Payment> payments = paymentService.getAll();
+
+        for (int i = 0; i < payments.size(); i++){
+            payments.get(i).setCompartmentObject(compartmentController.getCompartment(payments.get(i).getCompartment()));
+            if (payments.get(i).getProperty() == null){
+                payments.get(i).setPropertyObject(propertyController.getProperty(compartmentController.getCompartmentSpecificCompartment(payments.get(i).getCompartment()).getProperty()));
+            }else{
+                payments.get(i).setPropertyObject(propertyController.getProperty(payments.get(i).getProperty()));
+            }
+            payments.get(i).setTenantObject(tenantController.getTenant(compartmentController.getCompartmentSpecificCompartment(payments.get(i).getCompartment()).getTenant()));
+        }
+
+
+        return payments;
     }
 
     @GetMapping("/get-payments-for-specific-tenant/{id}")
