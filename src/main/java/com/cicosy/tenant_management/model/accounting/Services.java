d9df040043a76f447cb2c12b2043fcd807d9e41d
Mpumelelodube <dubempumelelo9@gmail.com>
2022-03-17 09:32:31 +0200

    invoicing.html module update

--- src/main/java/com/cicosy/tenant_management/model/accounting/Services.java
+++ src/main/java/com/cicosy/tenant_management/model/accounting/Services.java
@@ -24,8 +24,10 @@ public class Services {
     )
     private Long id;
     private String serviceName;
+    private double amount;
 
-    public Services(String serviceName) {
+    public Services(String serviceName, double amount) {
         this.serviceName = serviceName;
+        this.amount = amount;
     }
 }
--- src/main/resources/static/js/CustomInvoice.js
+++ src/main/resources/static/js/CustomInvoice.js
@@ -1,4 +1,4 @@
-function getInvoices(){
+function getInvoices() {
     $.ajax({
         url: 'http://localhost:8090/api/invoice/get-all-invoices',
         type: 'GET',
@@ -12,7 +12,7 @@ function getInvoices(){
             }
             console.log(response[1].paymentDate)
 
-            for (let i = response.length-1; i >= 0; i--) {
+            for (let i = response.length - 1; i >= 0; i--) {
                 let html = `<td>${response[i].dateIssued}</td>
                                                     <td>${response[i].id}</td>
                                                     <td>${response[i].compartment}</td>
@@ -63,6 +63,9 @@ function setAddPropertyDropDown() {
             }
         }
     })
+
+    let data = [];
+    localStorage.setItem('selectedCategories', JSON.stringify(data))
 }
 
 function setLettableSpaceDropdown() {
@@ -130,7 +133,7 @@ function setLettableSpaceDropdown() {
     }
 }
 
-function appendLettableSpace(){
+function appendLettableSpace() {
     let properties = JSON.parse(localStorage.getItem('i_properties'))
     let letaleSpaces = JSON.parse(localStorage.getItem('i_lettable'))
 
@@ -144,7 +147,7 @@ function appendLettableSpace(){
     }
 
     for (let i = 0; i < letaleSpaces.length; i++) {
-        if (letaleSpaces[i].id == space){
+        if (letaleSpaces[i].id == space) {
             let html = `<td>${letaleSpaces[i].dateIssued}</td>
                                                     <td>${letaleSpaces[i].compartmentNumber}</td>
                                                     <td>${letaleSpaces[i].floorArea} &#13217;</td>
@@ -161,7 +164,133 @@ function appendLettableSpace(){
 
 }
 
-function appendServices(){
-    let space = document.getElementById('multi-select2').value;
+function setServiceDropDown() {
+    $.ajax({
+        url: 'http://localhost:8090/api/services/get-all-services',
+        type: 'GET',
+        success: function (response) {
+            localStorage.setItem('i_services', JSON.stringify(response));
 
+            console.log(response)
+            let dropDown = document.getElementById("service-drop-down");
+
+            while (dropDown.hasChildNodes()) {
+                dropDown.removeChild(dropDown.firstChild);
+            }
+
+            let opt = document.createElement("option");
+
+            opt.text = "Select a service"
+            opt.setAttribute("value", `-1`)
+
+            dropDown.appendChild(opt);
+
+            for (let i = 0; i < response.length; i++) {
+                let option = document.createElement("option");
+
+                option.text = response[i].serviceName;
+                option.setAttribute("value", `${response[i].id}`)
+
+                dropDown.appendChild(option);
+            }
+        }
+    })
+}
+
+function appendServices() {
+    let response = JSON.parse(localStorage.getItem('i_services'))
+
+    let selectedServicesList  = JSON.parse(localStorage.getItem('selectedCategories'))
+    let service = document.getElementById('service-drop-down').value;
+
+    var verify = false
+
+    for (let i = 0 ; i <selectedServicesList.length; i++){
+        if (selectedServicesList[i] == service){
+            verify = true
+        }
+    }
+
+    if (verify){
+        console.log(true)
+    }else {
+        selectedServicesList.push(service)
+    }
+    console.log(selectedServicesList)
+    localStorage.setItem('selectedCategories', JSON.stringify(selectedServicesList))
+
+    let t_body = document.getElementById("t_body-3");
+
+    while (t_body.hasChildNodes()) {
+        t_body.removeChild(t_body.firstChild);
+    }
+
+
+    for (let i = 0; i < response.length; i++) {
+        for (let j = 0; j < selectedServicesList.length; j++){
+            if (response[i].id == selectedServicesList[j]) {
+                let html = `
+                                 <td>${response[i].serviceName}</td>
+                                 <td>
+                                    <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" value="${response[i].amount}">
+                                  </td>
+                                  <td><button type="button" onclick="removeService('${response[i].id}')" class="btn btn-danger btn-secondary btn-sm"><i class="bi bi-trash-fill"></i>remove </button></td>
+                                                    `
+
+                let tr = document.createElement('tr');
+                tr.innerHTML = html;
+
+                t_body.appendChild(tr)
+            }
+        }
+    }
+}
+
+function refreshServices(){
+    let response = JSON.parse(localStorage.getItem('i_services'))
+
+    let selectedServicesList  = JSON.parse(localStorage.getItem('selectedCategories'))
+
+
+    let t_body = document.getElementById("t_body-3");
+
+    while (t_body.hasChildNodes()) {
+        t_body.removeChild(t_body.firstChild);
+    }
+
+
+    for (let i = 0; i < response.length; i++) {
+        for (let j = 0; j < selectedServicesList.length; j++){
+            if (response[i].id == selectedServicesList[j]) {
+                let html = `
+                                 <td>${response[i].serviceName}</td>
+                                 <td>
+                                    <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" value="${response[i].amount}">
+                                  </td>
+                                  <td><button type="button" onclick="removeService('${response[i].id}')" class="btn btn-danger btn-secondary btn-sm">remove </button></td>
+                                                    `
+
+                let tr = document.createElement('tr');
+                tr.innerHTML = html;
+
+                t_body.appendChild(tr)
+            }
+        }
+    }
+}
+
+function removeService(id){
+    let selectedServicesList  = JSON.parse(localStorage.getItem('selectedCategories'))
+    console.log(selectedServicesList)
+
+    for (let i = 0; i < selectedServicesList.length; i++){
+        console.log(selectedServicesList[i])
+        if (id == selectedServicesList[i]) {
+            selectedServicesList.splice(i, 1)
+
+            localStorage.removeItem('selectedCategories')
+            localStorage.setItem('selectedCategories', JSON.stringify(selectedServicesList))
+            refreshServices()
+        }
+    }
 }
