69b3785b1c43ea3c17b60a44ede3424e9f9ae68b
mun168 <67210857+mun168@users.noreply.github.com>
2022-05-11 04:27:08 -0700

    fgh

--- src/main/java/com/cicosy/tenant_management/repository/document_management/Other_documentsRepo.java
+++ src/main/java/com/cicosy/tenant_management/repository/document_management/Other_documentsRepo.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface Other_documentsRepo extends JpaRepository<Other_documents,Long> {
 
-    @Query(value = "select other_file from other_documents  where document_name=?1 and tenant_id=?1  ",nativeQuery = true)
+    @Query(value = "select other_file from other_documents  where document_name=?2 and tenant_id=?1  ",nativeQuery = true)
     String findOther(String tenantId,String document_name);
 
 
